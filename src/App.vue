10b1b63361925f0d5e158ff721e723784f719bf9
jyotiyadav223133 <94034938+jyotiyadav223133@users.noreply.github.com>
2022-11-16 11:23:12 +0530

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <Navbar />
+      <router-view/>
   </div>
 </template>
+<script>
+import Navbar from './components/Navbar.vue'
 
+export default{
+  components:{
+    Navbar
+  }
+}
+</script>
+>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                Bootstrap
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+<script>
+export default{
+    
+}
+</script>
